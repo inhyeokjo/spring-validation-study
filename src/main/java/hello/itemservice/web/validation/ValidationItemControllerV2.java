4beb653df4ecd96164d6e7967caba1e8c38066d8
조인혁 <alzpqm@naver.com>
2022-01-14 21:59:55 +0900

    refactor: bindingResult.rejectValue, bindingResult.reject를 통해서 코드 간소화

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -91,21 +91,20 @@ public class ValidationItemControllerV2 {
 	private void getSpringBindingResult(Item item, BindingResult bindingResult) {
 		//검증 오류 결과 보관
 		if (!StringUtils.hasText(item.getItemName())) {
-			bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, new String[]{"required.item.itemName"}, null, null));
+			bindingResult.rejectValue("itemName", "required");
 		}
 		if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
-			bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, new String[]{"range.item.price"}, new Object[]{1000, 1000000}, null));
+			bindingResult.rejectValue("price", "range", new Object[]{1000,1000000}, null);
 		}
 		if (item.getQuantity() == null || item.getQuantity() >= 9999) {
-			bindingResult.addError(
-					new FieldError("item", "quantity", item.getQuantity(), false, new String[] {"max.item.quantity"}, new Object[] {9999}, null));
+			bindingResult.rejectValue("quantity", "max", new Object[]{9999}, null);
 		}
 
 		//특정 필드가 아닌 복합 룰 검증
 		if (item.getPrice() != null && item.getQuantity() != null) {
 			int resultPrice = item.getPrice() * item.getQuantity();
 			if (resultPrice < 10000) {
-				bindingResult.addError(new ObjectError("item", new String[]{"totalPriceMin"}, new Object[]{10000, resultPrice}, null));
+				bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
 			}
 		}
 	}
